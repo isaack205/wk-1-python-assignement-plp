46b67559a62d34ee4c7eaa05cdc871724516f0f6
isaack205 <kahuraisaac205@gmail.com>
2025-03-13 18:48:50 +0300

    Assignement week 1

---
+++ basic_calc.py
@@ -0,0 +1,24 @@
+# Function to perform the desired operation
+def calculate(num1, num2, operation):
+    if operation == '+':
+        return num1 + num2
+    elif operation == '-':
+        return num1 - num2
+    elif operation == '*':
+        return num1 * num2
+    elif operation == '/':
+        if num2 != 0:
+            return num1 / num2
+        else:
+            return "Error: Cannot divide by zero."
+    else:
+        return "Invalid operation"
+
+# Get input from the user
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+operation = input("Enter the operation (+, -, *, /): ").lower()
+
+# Perform calculation and display the result
+result = calculate(num1, num2, operation)
+print(f"The result is: {result}")
